8e28e16beb4879e875e4b69992c303fc1cbc1c4b
Kennethkilo <kennethwu.ubc@gmail.com>
2020-11-11 14:59:31 +0800

    First Update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/gadfgsdfgsdfgsdfg</code> and save to reload.
+          Kenneth's site is underconstruction boiiiiiiiiii.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/wukenneth/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My LinkedIn
         </a>
       </header>
     </div>
